c9dde6304db502571d399713c634d284f6371b6a
hyeun427 <hyeun427@gmail.com>
2022-03-14 22:38:53 +0900

    마이페이지 업로드

--- class/pages/quiz/01-faq/index.js
+++ class/pages/quiz/01-faq/index.js
@@ -1,44 +1,102 @@
-import styled from '@emotion/styled'
-
-
-
-
+import {
+	Background, Glassbox, Glass, Wrap, My, Profile, Name, Right, List, Menu, ThirdMenu, Line,
+	WrapQuestion, InnerQuestion, Question1, Question2, Arrow, Banner, IconWrap, Icon, IconName, IconNameMy
+} from '../../../styles/stylesheet'
 
 export default function AAAPage() {
 
-   //태그 이름의 첫 글자는 무조건 대문자로!
-    const Background = styled.div`
-    width: 640px;
-
-	border: 1px solid red;
-    `
-
-    const List = styled.div`
-    display: flex;
-	flex-direction: row;
-    `
-
-	const Menu = styled.div`
-	padding: 25px;
-	`
-
-   // 여기는 자바스크립트 쓰는 곳
-
 	return (
 		<Background>
-			<img src=''>
-			<div>마이 사진 임정아 화살표</div>
-			<List> 
+			<Glassbox>
+				<Glass src='/quiz/01-faq/ic-58-main-search-black_2022-03-14/ic-58-main-search-black.png'></Glass>
+			</Glassbox>
+			<Wrap>
+				<My>마이</My>
+				<Profile src='/quiz/01-faq/img-60-profile-image_2022-03-14/img-60-profile-image.png'></Profile>
+				<Name>임정아</Name>
+				<Right src='/quiz/01-faq/ic-28-arrow_2022-03-14/ic-28-arrow.png'></Right>
+			</Wrap>
+			<List>
 				<Menu>공지사항</Menu>
 				<Menu>이벤트</Menu>
-				<Menu>FAQ</Menu>
+				<ThirdMenu>FAQ</ThirdMenu>
 				<Menu>Q&A</Menu>
 			</List>
-			<div>라인</div>
-			<div>Q01-Q06</div>
-			<div>아래배너</div>
+			<Line></Line>
+
+
+			<WrapQuestion>
+				<InnerQuestion>
+					<Question1>Q.01</Question1>
+					<Question2>리뷰 작성은 어떻게 하나요?</Question2>
+				</InnerQuestion>
+				<Arrow src='/quiz/01-faq/ic-70-arrow-right_2022-03-14/ic-70-arrow-right.png'></Arrow>
+			</WrapQuestion>
+
+			<WrapQuestion>
+				<InnerQuestion>
+					<Question1>Q.02</Question1>
+					<Question2>리뷰 수정/삭제는 어떻게 하나요?</Question2>
+				</InnerQuestion>
+				<Arrow src='/quiz/01-faq/ic-70-arrow-right_2022-03-14/ic-70-arrow-right.png'></Arrow>
+			</WrapQuestion>
+
+			<WrapQuestion>
+				<InnerQuestion>
+					<Question1>Q.03</Question1>
+					<Question2>아이디/비밀번호를 잊어버렸어요.</Question2>
+				</InnerQuestion>
+				<Arrow src='/quiz/01-faq/ic-70-arrow-right_2022-03-14/ic-70-arrow-right.png'></Arrow>
+			</WrapQuestion>
+
+			<WrapQuestion>
+				<InnerQuestion>
+					<Question1>Q.04</Question1>
+					<Question2>회원탈퇴를 하고싶어요.</Question2>
+				</InnerQuestion>
+				<Arrow src='/quiz/01-faq/ic-70-arrow-right_2022-03-14/ic-70-arrow-right.png'></Arrow>
+			</WrapQuestion>
+
+			<WrapQuestion>
+				<InnerQuestion>
+					<Question1>Q.05</Question1>
+					<Question2>출발지 설정은 어떻게 하나요?</Question2>
+				</InnerQuestion>
+				<Arrow src='/quiz/01-faq/ic-70-arrow-right_2022-03-14/ic-70-arrow-right.png'></Arrow>
+			</WrapQuestion>
+
+			<WrapQuestion>
+				<InnerQuestion>
+					<Question1>Q.06</Question1>
+					<Question2>비밀번호를 변경하고 싶어요.</Question2>
+				</InnerQuestion>
+				<Arrow src='/quiz/01-faq/ic-70-arrow-right_2022-03-14/ic-70-arrow-right.png'></Arrow>
+			</WrapQuestion>
+
+			<Banner>
+				<IconWrap>
+					<Icon src='/quiz/01-faq/ic-58-main-home-unselected_2022-03-14/ic-58-main-home-unselected.png'></Icon>
+					<IconName>홈</IconName>
+				</IconWrap>
+
+				<IconWrap>
+					<Icon src='/quiz/01-faq/ic-58-main-location-unselected_2022-03-14/ic-58-main-location-unselected.png'></Icon>
+					<IconName>잇츠로드</IconName>
+				</IconWrap>
+
+				<IconWrap>
+					<Icon src='/quiz/01-faq/ic-58-main-like-unselected_2022-03-14/ic-58-main-like-unselected.png'></Icon>
+					<IconName>마이찜</IconName>
+				</IconWrap>
+
+				<IconWrap>
+					<Icon src='/quiz/01-faq/ic-58-main-my-selected_2022-03-14/ic-58-main-my-selected.png'></Icon>
+					<IconNameMy>마이</IconNameMy>
+				</IconWrap>
+
+			</Banner>
 	
-    </Background>
+		</Background>
     
-    )
+	)
 }
---
+++ class/styles/stylesheet.js
@@ -0,0 +1,189 @@
+import styled from '@emotion/styled'
+
+export const Background = styled.div`
+width: 640px;
+margin-top: 50px;
+padding: 36px 48px 0px 50px; 
+`
+
+export const Glassbox = styled.div`
+width: 100%;
+height: 40px;
+display: flex;
+flex-direction: row;
+justify-content: flex-end;
+`
+
+export const Glass = styled.img`
+width: 32px;
+height: 32px;
+object-fit: contain;
+`
+
+export const Wrap = styled.div`
+display: flex;
+flex-direction: row;
+align-items: center;
+line-height: 60px;
+text-align: center;
+padding-bottom: 60px;
+`
+
+export const My = styled.div`
+margin-right: 295px;
+font-family: SpoqaHanSans;
+font-size: 40px;
+font-weight: bold;
+font-stretch: normal;
+font-style: normal;
+line-height: 1.07;
+letter-spacing: -1.33px;
+color: #1f1f1f;
+`
+
+export const Profile = styled.img`
+width: 60px;
+height: 60px;
+`
+
+export const Name = styled.div`
+font-family: SpoqaHanSans;
+font-size: 24px;
+font-weight: bold;
+font-stretch: normal;
+font-style: normal;
+line-height: 1.78;
+letter-spacing: -0.8px;
+color: #1f1f1f;
+margin-left: 5px;
+`
+
+export const Right = styled.img`
+width: 28px;
+height: 28px;
+padding-left: 4px;
+margin-left: 4px;
+`
+
+export const List = styled.div`
+display: flex;
+flex-direction: row;
+`
+
+export const Menu = styled.div`
+margin-right: 50px;
+font-family: SpoqaHanSans;
+font-size: 28px;
+font-weight: bold;
+font-stretch: normal;
+font-style: normal;
+line-height: 1.87;
+letter-spacing: normal;
+color: #cacaca;
+`
+
+export const ThirdMenu = styled.div`
+font-family: SpoqaHanSans;
+margin-right: 50px;
+font-family: SpoqaHanSans;
+font-size: 28px;
+font-weight: bold;
+font-stretch: normal;
+font-style: normal;
+line-height: 1.87;
+letter-spacing: normal;
+color: #ff1b6d;
+`
+
+// 부모 요소에 왼쪽 패딩을 준 상태에서 Line만 부모요소의 패딩을 없앨 수 있나??
+export const Line = styled.div`
+padding-top: 60px;
+margin-bottom: 29px;
+border-bottom: solid 1px #cacaca;
+`
+
+export const WrapQuestion = styled.div`
+width: 550px;
+height: 60px;
+margin-bottom: 50px;
+display: flex;
+align-items: center;
+`
+
+export const InnerQuestion = styled.div`
+width: 490px;
+height: 60px;
+`
+
+export const Question1 = styled.div`
+font-family: SpoqaHanSans;
+font-size: 18px;
+font-weight: normal;
+font-stretch: normal;
+font-style: normal;
+letter-spacing: normal;
+color: #adadad;
+`
+
+export const Question2 = styled.div`
+font-family: SpoqaHanSans;
+font-size: 24px;
+font-weight: normal;
+font-stretch: normal;
+font-style: normal;
+letter-spacing: normal;
+color: #444444;
+`
+
+export const Arrow = styled.img`
+width: 60px;
+height: 60px;
+`
+
+export const Banner = styled.div`
+width: 100%;
+height: 96px;
+margin: 5px 0px 10px;
+padding-top: 8px;
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+border-top: 1px solid #dcdcdc;
+`
+
+export const IconWrap = styled.div`
+width: 70px;
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+
+export const Icon = styled.img`
+width: 58px;
+height: 58px;
+`
+
+export const IconName = styled.div`
+font-family: SpoqaHanSans;
+font-size: 16px;
+font-weight: normal;
+font-stretch: normal;
+font-style: normal;
+line-height: 2.13;
+letter-spacing: normal;
+text-align: center;
+color: #adadad;
+`
+
+
+export const IconNameMy = styled.div`
+font-family: SpoqaHanSans;
+font-size: 16px;
+font-weight: bold;
+font-stretch: normal;
+font-style: normal;
+line-height: 2.13;
+letter-spacing: normal;
+text-align: center;
+color: #ff1b6d
+`
