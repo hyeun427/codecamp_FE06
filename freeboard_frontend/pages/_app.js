e479bc07375a604edecbcb3047e7d5f85ba81511
hyeun427 <hyeun427@gmail.com>
2022-03-19 19:26:37 +0900

    프리보드 수정

--- freeboard_frontend/pages/_app.js
+++ freeboard_frontend/pages/_app.js
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
 function MyApp({ Component, pageProps }) {
-
   //'아래 주소가 실제 백엔드서버 컴퓨터야' 라는 걸 알려줌
   const client = new ApolloClient({
     uri: "http://backend06.codebootcamp.co.kr/graphql",
--- freeboard_frontend/pages/boards/new/index.js
+++ freeboard_frontend/pages/boards/new/index.js
@@ -2,15 +2,15 @@ import {
     Wrapper,
     Title,
     WriterWrapper,
+    InputWrapper,
+    Label,
     Writer,
     Password,
-    Label,
-    InputWrapper,
     Subject,
     Contents,
-    ZipcodeWrapper,
-    Zipcode,
-    SearchButton,
+    CodeWrapper,
+    Code,
+    SearchCode,
     Address,
     Youtube,
     ImageWrapper,
@@ -22,120 +22,147 @@ import {
     SubmitButton,
     Error
 } from '../../../styles/emotion'
-import { useState } from 'react'
 
+import { useState } from 'react'
+import { useMutation, gql } from '@apollo/client'
+
+export const CREATE_BOARD = gql`
+    mutation createBoard($createBoardInput: CreateBoardInput!) {
+        createBoard(createBoardInput: $createBoardInput) {
+            _id
+        }
+    }
+`;
+
+    
 export default function BoardsNewPage(){
     const [writer, setWriter] = useState("");
     const [password, setPassword] = useState("");
-    const [title, setTitle] = useState("");
+    const [subject, setSubject] = useState("");
     const [contents, setContents] = useState("");
 
     const [writerError, setWriterError] = useState("");
     const [passwordError, setPasswordError] = useState("");
-    const [titleError, setTitleError] = useState("");
+    const [subjectError, setSubjectError] = useState("");
     const [contentsError, setContentsError] = useState("");
+
+    const [createBoard] = useMutation(CREATE_BOARD);
 
     const onChangeWriter = (event) => {
         setWriter(event.target.value);
-        if (event.target.value !== "") {
-        setWriterError("");
+        if (writer !== "") {
+            setWriterError("")
         }
     };
 
     const onChangePassword = (event) => {
         setPassword(event.target.value);
-        if (event.target.value !== "") {
-        setPasswordError("");
-    }
+        if (password !== "") {
+            setPasswordError("")
+        }
     };
 
-    const onChangeTitle = (event) => {
-        setTitle(event.target.value);
-        if (event.target.value !== "") {
-        setTitleError("");
+    const onChangeSubject = (event) => {
+        setSubject(event.target.value);
+        if (subject !== "") {
+            setSubjectError("")
         }
     };
 
     const onChangeContents = (event) => {
         setContents(event.target.value);
-        if (event.target.value !== "") {
-        setContentsError("");
+        if (contents !== "") {
+            setContentsError("")
         }
     };
 
     const onClickSubmit = async () => {
-    if (writer === "") {
-        setWriterError("작성자를 입력해주세요.");
-    }
-    if (password === "") {
-        setPasswordError("비밀번호를 입력해주세요.");
-    }
-    if (title === "") {
-        setTitleError("제목을 입력해주세요.");
-    }
-    if (contents === "") {
-        setContentsError("내용을 입력해주세요.");
-    }
-    if(writer !== "" && password !== "" && title !== "" && contents !== "") {
-        alert("게시물 등록에 성공하였습니다!")
-    }
+        if (writer === "") {
+            setWriterError("작성자를 입력하세요.")
+        }
+        if (password === "") {
+            setPasswordError("비밀번호를 입력하세요.")
+        }
+        if (subject === "") {
+            setSubjectError("제목을 입력하세요.")
+        }
+        if (contents === "") {
+            setContentsError("내용을 입력하세요.")
+        }
+        if (writer !== "" && password !== "" && subject !== "" && contents !== "") {
+            try {
+                const result = await createBoard({
+                    variables: {
+                        createBoardInput: {
+                            writer: writer,
+                            password: password,
+                            title: subject,
+                            contents: contents
+                        }
+                    }
+                })
+                console.log(result)
+                alert("게시물 등록에 성공하였습니다!")
+            } catch(error) {
+            console.log(error.message)
+            }
+        }
     };
 
     return (
-    <Wrapper>
-        <Title>게시판 등록</Title>
-        <WriterWrapper>
+        <Wrapper>
+            <Title>게시물 등록</Title>
+            <WriterWrapper>
+                <InputWrapper>
+                    <Label>작성자</Label>
+                    <Writer type='text' placeholder='이름을 적어주세요.' onChange={onChangeWriter}></Writer>
+                    <Error>{writerError}</Error>
+                </InputWrapper>
+                <InputWrapper>
+                    <Label>비밀번호</Label>
+                    <Password type='password' placeholder='비밀번호를 작성해주세요.' onChange={onChangePassword}></Password>
+                    <Error>{passwordError}</Error>
+                </InputWrapper>
+            </WriterWrapper>
             <InputWrapper>
-                <Label>작성자</Label>
-                <Writer type="text" placeholder="이름을 적어주세요." onChange={onChangeWriter} />
-                <Error>{writerError}</Error>
+                <Label>제목</Label>
+                <Subject type='text' placeholder='제목을 작성해주세요.' onChange={onChangeSubject}></Subject>
+                <Error>{subjectError}</Error>
             </InputWrapper>
             <InputWrapper>
-                <Label>비밀번호</Label>
-                <Password type="password" placeholder="비밀번호를 작성해주세요." onChange={onChangePassword} />
-                <Error>{passwordError}</Error>
+                <Label>내용</Label>
+                <Contents type='text' placeholder='내용을 작성해주세요.' onChange={onChangeContents}></Contents>
+                <Error>{contentsError}</Error>
             </InputWrapper>
-        </WriterWrapper>
-        <InputWrapper>
-            <Label>제목</Label>
-            <Subject type="text" placeholder="제목을 작성해주세요." onChange={onChangeTitle} />
-            <Error>{titleError}</Error>
-        </InputWrapper>
-        <InputWrapper>
-            <Label>내용</Label>
-            <Contents placeholder="내용을 작성해주세요." onChange={onChangeContents} />
-            <Error>{contentsError}</Error>
-        </InputWrapper>
-        <InputWrapper>
-            <Label>주소</Label>
-            <ZipcodeWrapper>
-                <Zipcode placeholder="07250" />
-                <SearchButton>우편번호 검색</SearchButton>
-            </ZipcodeWrapper>
-            <Address />
-            <Address />
-        </InputWrapper>
-        <InputWrapper>
-            <Label>유튜브</Label>
-            <Youtube placeholder="링크를 복사해주세요." />
-        </InputWrapper>
-        <ImageWrapper>
-            <Label>사진첨부</Label>
-            <UploadButton>+</UploadButton>
-            <UploadButton>+</UploadButton>
-            <UploadButton>+</UploadButton>
-        </ImageWrapper>
-        <OptionWrapper>
-            <Label>메인설정</Label>
-            <RadioButton type="radio" id="youtube" name="radio-button" />
-            <RadioLabel htmlFor="youtube">유튜브</RadioLabel>
-            <RadioButton type="radio" id="image" name="radio-button" />
-            <RadioLabel htmlFor="image">사진</RadioLabel>
-        </OptionWrapper>
-        <ButtonWrapper>
-            <SubmitButton onClick={onClickSubmit}>등록하기</SubmitButton>
-        </ButtonWrapper>
-    </Wrapper>
+            <InputWrapper>
+                <Label>주소</Label>
+                <CodeWrapper>
+                    <Code placeholder="07250"></Code>
+                    <SearchCode>우편번호 검색</SearchCode>
+                </CodeWrapper>
+                <Address></Address>
+                <Address />
+            </InputWrapper>
+            <InputWrapper>
+                <Label>유튜브</Label>
+                <Youtube placeholder='링크를 복사해주세요.'></Youtube>
+            </InputWrapper>
+            <ImageWrapper>
+                <Label>사진첨부</Label>
+                <UploadButton>+</UploadButton>
+                <UploadButton>+</UploadButton>
+                <UploadButton>+</UploadButton>
+            </ImageWrapper>
+            <OptionWrapper>
+                <Label>메인설정</Label>
+                <RadioButton type="radio" />
+                <RadioLabel>유튜브</RadioLabel>
+                <RadioButton type="radio" />
+                <RadioLabel>사진</RadioLabel>
+            </OptionWrapper>
+            <ButtonWrapper>
+                <SubmitButton onClick={onClickSubmit}>등록하기</SubmitButton>
+            </ButtonWrapper>
+        </Wrapper>
     )
 }
-
