901baabbcc0e3a0bbe6857b3e6f9c8f5c33d3df2
hyeun427 <hyeun427@gmail.com>
2022-03-20 15:01:32 +0900

    포트폴리오 중간 업로드

--- freeboard_frontend/pages/boards/[boardId]/index.js
+++ freeboard_frontend/pages/boards/[boardId]/index.js
@@ -1,7 +1,9 @@
 import {
+	OutWrapper,
 	Wrapper,
 	ProfileWrapper,
 	Profile,
+	ProfileDetail,
 	Photo,
 	Writer,
 	Date,
@@ -15,74 +17,149 @@ import {
 	YoutubeWrapper,
 	Youtube,
 	LikeWrapper,
-	Like,
-	DisLike,
-	List,
-	Edit,
-	Delete
+	InnerWrapper,
+	ThumbsButton,
+	LikeCount,
+	DislikeCount,
+	ButtonWrapper,
+	Button
 } from "../../../styles/stylesheet"
-import { useQuery, gql } from '@apollo/client'
-import { useRouter } from 'next/router'
+// import { useQuery, gql } from '@apollo/client'
+// import { useRouter } from 'next/router'
 
-const FETCH_BOARD = gql `
-	query fetchBoard($boardId: ID!){
-		fetchBoard(boardId: $boardId){
+// const FETCH_BOARD = gql `
+// 	query fetchBoard($boardId: ID!){
+// 		fetchBoard(boardId: $boardId){
 
-			_id
-			writer
-			title
-			contents
-			youtubeUrl
-			likeCount
-			disLikeCount
-			images
-			boardAddress
-			user
-			createdAt
-			updatedAt
-			deletedAt
-		}
-	}
-`
+// 			_id
+// 			writer
+// 			title
+// 			contents
+// 			youtubeUrl
+// 			likeCount
+// 			disLikeCount
+// 			images
+// 			boardAddress {
+// 				_id
+// 				zipcode
+// 				address
+// 				addressDetail
+// 				createdAt
+// 				updatedAt
+// 				deletedAt
+// 			}
+// 			user{
+// 				_id 
+// 				email
+// 				name
+// 				picture
+// 				userPoint
+// 				createdAt
+// 				updatedAt
+// 				deletedAt
+// 			}
+// 			createdAt
+// 			updatedAt
+// 			deletedAt
+// 		}
+// 	}
+// `
 
 export default function StaticRoutedPage() {
-	const router = useRouter
-	console.log(router)
+	// const router = useRouter
+	// console.log(router)
 
-	const { data } = useQuery(FETCH_BOARD, {
-		variables: {boardId: router.query.boardId}
-	})
+	// const { data } = useQuery(FETCH_BOARD, {
+	// 	variables: {boardId: router.query.boardId}
+	// })
 
 	return (
-		<Wrapper>
-			<ProfileWrapper>
-				<Profile>
-					<Photo>프로필사진</Photo>
-					<Writer> {data?.fetchBoard.writer} </Writer>
-					<Date> {data?.fetchBoard.createdAt} </Date>
-				</Profile>
-				<IconWrapper>
-					<Share></Share>
-					<Spot></Spot>
-				</IconWrapper>
-			</ProfileWrapper>
-			<ContentWrapper>
-				<Title> {data?.fetchBoard.title} </Title>
-				<ContentImage> {data?.fetchBoard.images} </ContentImage>
-				<Content> {data?.fetchBoard.contents} </Content>
-			</ContentWrapper>
-			<YoutubeWrapper>
-				<Youtube> {data?.fetchBoard.youtubeUrl} </Youtube>
-			</YoutubeWrapper>
-			<LikeWrapper>
-				<Like> 이미지 넣기 </Like>
-				<DisLike> 이미지 넣기 </DisLike>
-			</LikeWrapper>
+		// <Wrapper>
+		// 	<ProfileWrapper>
+		// 		<Profile>
+		// 			<Photo>프로필사진</Photo>
+		// 			<Writer> {data?.fetchBoard.writer} </Writer>
+		// 			<Date> {data?.fetchBoard.createdAt} </Date>
+		// 		</Profile>
+		// 		<IconWrapper>
+		// 			<Share></Share>
+		// 			<Spot></Spot>
+		// 		</IconWrapper>
+		// 	</ProfileWrapper>
+		// 	<ContentWrapper>
+		// 		<Title> {data?.fetchBoard.title} </Title>
+		// 		<ContentImage> {data?.fetchBoard.images} </ContentImage>
+		// 		<Content> {data?.fetchBoard.contents} </Content>
+		// 	</ContentWrapper>
+		// 	<YoutubeWrapper>
+		// 		<Youtube> {data?.fetchBoard.youtubeUrl} </Youtube>
+		// 	</YoutubeWrapper>
+		// 	<LikeWrapper>
+		// 		<InnerWrapper>
+		// 			<ThumbsButton></ThumbsButton>
+		// 			<LikeCount>1920</LikeCount>
+		// 		</InnerWrapper>
+		// 		<InnerWrapper>
+		// 			<ThumbsButton></ThumbsButton>
+		// 			<DislikeCount>1920</DislikeCount>
+		// 		</InnerWrapper>
+		// 	</LikeWrapper>
+		// 	<InputWrapper>
+		// 		<Input>목록으로</Input>
+		// 		<Input>수정하기</Input>
+		// 		<Input>삭제하기</Input>
+		// 	</InputWrapper>
+		// </Wrapper>
+		<OutWrapper>
+			<Wrapper>
+				<ProfileWrapper>
+					<Profile>
+						<Photo src="/img/profile.png" />
+						<ProfileDetail>
+							<Writer>작성자</Writer>
+							<Date>Date:2022.03.20</Date>
+						</ProfileDetail>
+					</Profile>
+					<IconWrapper>
+						<Share src="/img/share.png" ></Share>
+						<Spot src="/img/spot.png" />
+					</IconWrapper>
+				</ProfileWrapper>
+
+
+
+				<ContentWrapper>
+					<Title>게시글 제목입니다.</Title>
+					<ContentImage>첨부한 이미지 들어가는 곳</ContentImage>
+					<Content>글쓴 내용 들어가는 곳</Content>
+				</ContentWrapper>
+
+
+				<YoutubeWrapper>
+					<Youtube>영상 들어가는 곳</Youtube>
+				</YoutubeWrapper>
+
+
+
+				<LikeWrapper>
+					<InnerWrapper>
+						<ThumbsButton>좋아요이미지</ThumbsButton>
+						<LikeCount>1920</LikeCount>
+					</InnerWrapper>
+					<InnerWrapper>
+						<ThumbsButton>싫어요이미지</ThumbsButton>
+						<DislikeCount>1920</DislikeCount>
+					</InnerWrapper>
+				</LikeWrapper>
+			</Wrapper>
+
+
+
 			<ButtonWrapper>
-				<List>목록으로</List>
-				<Edit>수정하기</Edit>
-				<Delete>삭제하기</Delete>
+				<Button>목록으로</Button>
+				<Button>수정하기</Button>
+				<Button>삭제하기</Button>
 			</ButtonWrapper>
-		</Wrapper>
+		</OutWrapper>
 	);
 }
--- freeboard_frontend/styles/stylesheet.js
+++ freeboard_frontend/styles/stylesheet.js
@@ -1,5 +1,15 @@
 import styled from "@emotion/styled"
 
+// OutWrapper는 아래 댓글 창까지 만들 때 수정/삭제 할 것.
+export const OutWrapper = styled.div`
+	width: 1200px;
+	padding-bottom: 87px;
+
+	//완성 후 삭제 할 것
+	margin: 100px 0px 0px 100px;
+	border-bottom: 1px solid #BDBDBD;
+`
+
 export const Wrapper = styled.div`
 	width: 1200px;
 	padding : 80px 102px;
@@ -7,20 +17,31 @@ export const Wrapper = styled.div`
 `
 export const ProfileWrapper = styled.div`
 	width: 996px;
-	height: 24px;
+
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	padding-bottom: 20px;
 	border-bottom: 1px solid #BDBDBD;
 `
-export const Profile = styled.image`
-	width: 126px;
-	height: 24px;
-	`
-export const Photo = styled.div`
+export const Profile = styled.div`
+
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+`
+export const Photo = styled.img`
 	height: 46.67px;
 	width: 46.67px;
+	margin-right: 16px;
+`
+
+export const ProfileDetail = styled.div`
+	display:flex;
+	flex-direction: column;
+
 `
 export const Writer = styled.div`
-	width: 67px;
-	height: 36px;
 	font-style: normal;
 	font-weight: 500;
 	font-size: 24px;
@@ -28,28 +49,122 @@ export const Writer = styled.div`
 export const Date = styled.div`
 	width: 126px;
 	height: 24px;
+	color: #828282;
 `
+
 export const IconWrapper = styled.div`
-	width: 32px;
-	height: 32px;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
 `
-export const Share = styled.image`
+export const Share = styled.img`
+	height: 13px;
+	margin-right: 29px;
 `
-export const Spot = styled.image`
+export const Spot = styled.img`
+	width: 22px;
 `
+
 export const ContentWrapper = styled.div`
 	padding-top: 80px;
 `
 export const Title = styled.div`
-	
+width: 285px;
+height: 54px;
+
+font-family: 'Noto Sans CJK KR';
+font-style: normal;
+font-weight: 700;
+font-size: 36px;
+line-height: 53px;
 `
-export const ContentImage = styled.div``
-export const Content = styled.div``
-export const YoutubeWrapper = styled.div``
-export const Youtube = styled.div``
-export const LikeWrapper = styled.div``
-export const Like = styled.div``
-export const DisLike = styled.div``
-export const List = styled.div``
-export const Edit = styled.div``
-export const Delete = styled.div``
+export const ContentImage = styled.div`
+	width: 996px;
+	height: 480px;
+	// routing 파일에서 작성한 이미지 가져올거라 아래 코드는 작성 안해도 될 듯.
+	// background: url(nainoa-shizuru-NcdG9mK3PBY-unsplash.jpg);
+`
+export const Content = styled.div`
+	width: 996px;
+	height: 96px;
+
+	font-family: 'Noto Sans CJK KR';
+	font-style: normal;
+	font-weight: 400;
+	font-size: 16px;
+	line-height: 24px;
+`
+
+
+
+
+
+
+
+
+
+
+export const YoutubeWrapper = styled.div`
+	width: 996px;
+	height: 240px;
+
+	padding: 120px 255px;
+`
+export const Youtube = styled.div`
+	width: 486px;
+	height: 240px;
+	// filter는 뭐지??
+	// filter: drop-shadow(0px 5px 20px rgba(0, 0, 0, 0.2));
+`
+export const LikeWrapper = styled.div`
+	width: 100%;
+	height: 51px;
+
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+
+	border: 1px solid red;
+`
+export const InnerWrapper = styled.div`
+	//width: 40px;
+	//height: 51px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+
+	background-color: yellow;
+`
+export const ThumbsButton = styled.button`
+	//width: 22px;
+	//height: 20px;
+`
+export const LikeCount = styled.div`
+	width: 40px;
+	height: 27px;
+	color: #FFD600;
+;
+`
+
+export const DislikeCount = styled.div`
+	width: 40px;
+	height: 27px;
+	color: #828282;
+`
+export const ButtonWrapper = styled.div`
+	width: 585px;
+	//height: 45px;
+	margin-left: 305px;
+	padding-top: 101px;
+
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+
+	border: 1px solid red;
+`
+export const Button = styled.button`
+	width: 179px;
+	height: 45px;
+`
