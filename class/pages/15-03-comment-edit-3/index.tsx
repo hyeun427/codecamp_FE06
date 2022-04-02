d4e135a5f7ff7d4fce3a382aafd88146dd4e5d34
hyeun427 <hyeun427@gmail.com>
2022-04-02 16:43:50 +0900

    토요일 업로드1

---
+++ class/pages/15-03-comment-edit-3/index.tsx
@@ -0,0 +1,42 @@
+import { useQuery, gql } from "@apollo/client";
+import BoardCommentItem from "../../src/components/units/board/15-board-comment";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards {
+    fetchBoards {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function MapBoardPage() {
+  // const [myIndex, setMyIndex] = useState([
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  //   false,
+  // ]);
+  const { data } = useQuery(FETCH_BOARDS);
+
+  // const onClickEdit = (event) => {
+  //   const aaa = myIndex;
+  //   aaa[event.target.id] = true;
+  // };
+
+  return (
+    <div>
+      {data?.fetchBoards.map((el, index) => (
+        <BoardCommentItem key={el._id} el={el}/>
+      ))}
+    </div>
+  );
+}
