61241862c41352e74cc1960db9e92faf3b4d6855
hyeun427 <hyeun427@gmail.com>
2022-04-09 20:49:42 +0900

    토요일

--- freeboard_frontend/pages/boards/index.tsx
+++ freeboard_frontend/pages/boards/index.tsx
@@ -1,8 +1,14 @@
 // boards
 // 게시물 목록 페이지
 
+import { Pagination } from "antd";
 import BoardList from "../../src/components/units/board/list/BoardList.container";
 
 export default function BoardsPage() {
-  return <BoardList />;
+  return (
+    <>
+      <BoardList />
+      <Pagination defaultCurrent={1} total={50} />
+    </>
+  );
 }
